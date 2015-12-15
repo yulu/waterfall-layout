720f89782dcedd1c82ad51e7a796c8ce8ce92876
Yu Lu <yulu8798@gmail.com>
2015-12-16 00:35:00 +0800

    add example

--- app/src/main/java/me/littlecheesecake/waterfalllayout/MainActivity.java
+++ app/src/main/java/me/littlecheesecake/waterfalllayout/MainActivity.java
@@ -1,52 +1,87 @@
 package me.littlecheesecake.waterfalllayout;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import me.littlecheesecake.waterfalllayout.view.StretchyImageView;
+import me.littlecheesecake.waterfalllayoutview.MultiColumnListView;
 
 public class MainActivity extends AppCompatActivity {
+    private WFAdapter mAdapter;
+    private MultiColumnListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        listView = (MultiColumnListView) findViewById(R.id.list);
+        List<Integer> imageList = new ArrayList<>();
+        Integer[] list = new Integer[]{
+                R.mipmap.pic1, R.mipmap.pic2, R.mipmap.pic3,
+                R.mipmap.pic4, R.mipmap.pic5, R.mipmap.pic6,
+                R.mipmap.pic7, R.mipmap.pic8, R.mipmap.pic9
+        };
+        Collections.addAll(imageList, list);
+        mAdapter = new WFAdapter(this, imageList);
+        listView.setAdapter(mAdapter);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    private static class WFAdapter extends ArrayAdapter<Integer> {
+        private Activity context;
+        private List<Integer> imageList;
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        public WFAdapter(Activity context, List<Integer> imageList) {
+            super(context, R.layout.sample_item, imageList);
+            this.imageList = imageList;
+            this.context = context;
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view;
+
+            if (convertView == null) {
+                LayoutInflater inflater = context.getLayoutInflater();
+                view = inflater.inflate(R.layout.sample_item, null);
+
+                final Holder holder = new Holder();
+                holder.imageView = (StretchyImageView) view.findViewById(R.id.sample_item_image);
+
+                view.setTag(holder);
+            } else {
+                view =convertView;
+            }
+
+            Holder holder = (Holder) view.getTag();
+
+            Picasso.with(context)
+                    .load(imageList.get(position))
+                    .placeholder(R.drawable.empty_image)
+                    .tag(context)
+                    .into(holder.imageView);
+
+            return view;
+        }
+
+        private class Holder {
+            public StretchyImageView imageView;
+        }
     }
 }
